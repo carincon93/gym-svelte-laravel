e722661ecb982e6b1b4a0b36986584128bf54943
Cristian Rincón <carincon93@gmail.com>
2021-10-10 14:46:33 -0500

    register modified

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Sesion;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
@@ -44,6 +45,11 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+        ]);
+
+        Sesion::create([
+            'user_id' => $user->id,
+            'numero_sesion' => 1
         ]);
 
         event(new Registered($user));
